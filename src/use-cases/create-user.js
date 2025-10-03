eadc3c2db7e80344af43afbfc5f9668f8e218b63
Philipe Faria <philipefaria61@gmail.com>
2025-10-03 20:39:49 -0300

    feat: single email validation

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -2,9 +2,20 @@ import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
 
 import { PostgresCreateUserRepository } from "../repositories/postgres/create-user.js";
+import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
 
 class CreateUserUseCase {
   async execute(createUserParams) {
+    const postgresGetUserByEmailRepository =
+      new PostgresGetUserByEmailRepository();
+
+    const userWithProvidedEmail =
+      await postgresGetUserByEmailRepository.execute(createUserParams.email);
+
+    if (userWithProvidedEmail) {
+      throw new Error("The provided e-mail is already in use.");
+    }
+
     const userId = uuidv4();
 
     const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
