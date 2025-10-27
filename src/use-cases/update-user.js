8fed7b875219c59eb255c4277f5885a3ea8c4825
Philipe Faria <philipefaria61@gmail.com>
2025-10-27 11:21:17 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+
+import { EmailAlreadyInUseError } from "../errors/user.js";
+
+import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
+import { PostgresUpdateUserRepository } from "../repositories/postgres/update-user.js";
+
+class UpdateUserUseCase {
+  async execute(userId, updateUserParams) {
+    if (updateUserParams.email) {
+      const postgresGetUserByEmailRepository =
+        new PostgresGetUserByEmailRepository();
+
+      const userWithProvidedEmail =
+        await postgresGetUserByEmailRepository.execute(updateUserParams.email);
+
+      if (userWithProvidedEmail) {
+        throw new EmailAlreadyInUseError(updateUserParams.email);
+      }
+    }
+
+    const user = {
+      ...updateUserParams,
+    };
+
+    if (updateUserParams.password) {
+      const hashedPassword = await bcrypt.hash(updateUserParams.password, 10);
+
+      user.password = hashedPassword;
+    }
+
+    const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+
+    const updatedUser = await postgresUpdateUserRepository.execute(
+      userId,
+      user
+    );
+
+    return updatedUser;
+  }
+}
+
+export { UpdateUserUseCase };
